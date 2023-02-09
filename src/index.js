e645124e1e07825d176d26517f2c99f7a81ac37c
Dzianis Perahudau <tvigger@yandex.ru>
2023-02-09 22:06:53 +0300

    init: start Brackets Task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < str.length; i++) {
+  for (let x = 0; x < bracketsConfig.length; x++) {
+if (str[i].includes[x[0]])
+  }
 }

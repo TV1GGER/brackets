8fbc724c251d3444e482b8d3bd9e04bd7da9a63f
Dzianis Perahudau <tvigger@yandex.ru>
2023-02-12 13:47:12 +0300

    feat: add Brackets task code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  for (let i = 0; i < str.length; i++) {
-  for (let x = 0; x < bracketsConfig.length; x++) {
-if (str[i].includes[x[0]])
-  }
-}
+
+  if((str.length===1) || (str.length%2===1) || (str.length===undefined)) {
+     return  false;
+   };
+   let count = 2;
+   let resultStack = [];
+   let arrRev = [];
+   let firstArr = [];
+   let endArr = [];
+   let topElement = resultStack[resultStack.length-1];
+   for (let p=0; p<bracketsConfig.length; p++){
+     firstArr.push(bracketsConfig[p][0]);
+     endArr.push(bracketsConfig[p][1]);
+   };
+  for (let z=0; z<bracketsConfig.length; z++){
+   arrRev.push(bracketsConfig[z].reverse());
+  };
+  let map1 = new Map(arrRev);
+   for (let x = 0; x < str.length; x++) {
+     let currentStr = str[x];
+        if(firstArr.includes(currentStr)){
+         resultStack.push(currentStr);        
+         }
+         if((endArr.includes(currentStr)) && (map1.get(currentStr) !== resultStack.pop())){
+           return false;
+           }
+          if((firstArr.includes(currentStr)) && (endArr.includes(currentStr)) && (count%2===0)){
+            resultStack.push(currentStr); 
+     count++;
+             }
+          if((firstArr.includes(currentStr)) && (endArr.includes(currentStr)) && (count%2===1)){
+     resultStack.pop()
+     count++;
+     }
+         }
+       return resultStack.length === 0;
+       }
+ 

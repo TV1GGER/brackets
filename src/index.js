1e3560a5f3ea7cac701ff15fe25bda21a89ede35
Dzianis Perahudau <tvigger@yandex.ru>
2023-02-24 21:57:08 +0300

    fix: fix brackets code js

--- src/index.js
+++ src/index.js
@@ -3,30 +3,32 @@ module.exports = function check(str, bracketsConfig) {
   if((str.length===1) || (str.length%2===1) || (str.length===undefined)) {
      return  false;
    };
+   let cloneBracketsConfig = JSON.parse(JSON.stringify(bracketsConfig));
+   let cloneBracketsConfig2 = JSON.parse(JSON.stringify(bracketsConfig));
+   let cloneStr = JSON.parse(JSON.stringify(str));
    let count = 2;
    let resultStack = [];
    let arrRev = [];
    let firstArr = [];
    let endArr = [];
-   let topElement = resultStack[resultStack.length-1];
    for (let p=0; p<bracketsConfig.length; p++){
-     firstArr.push(bracketsConfig[p][0]);
-     endArr.push(bracketsConfig[p][1]);
+     firstArr.push(cloneBracketsConfig[p][0]);
+     endArr.push(cloneBracketsConfig[p][1]);
    };
   for (let z=0; z<bracketsConfig.length; z++){
-   arrRev.push(bracketsConfig[z].reverse());
+   arrRev.push(cloneBracketsConfig2[z].reverse());
   };
   let map1 = new Map(arrRev);
    for (let x = 0; x < str.length; x++) {
      let currentStr = str[x];
         if(firstArr.includes(currentStr)){
-         resultStack.push(currentStr);        
+         resultStack.push(cloneStr[x]);        
          }
          if((endArr.includes(currentStr)) && (map1.get(currentStr) !== resultStack.pop())){
            return false;
            }
           if((firstArr.includes(currentStr)) && (endArr.includes(currentStr)) && (count%2===0)){
-            resultStack.push(currentStr); 
+            resultStack.push(cloneStr[x]); 
      count++;
              }
           if((firstArr.includes(currentStr)) && (endArr.includes(currentStr)) && (count%2===1)){
@@ -35,5 +37,5 @@ module.exports = function check(str, bracketsConfig) {
      }
          }
        return resultStack.length === 0;
-       }
+       } 
  
